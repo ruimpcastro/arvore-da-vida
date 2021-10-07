89ac9263e58219024fcaaf81bbc4f66ba039e807
Rui Castro <rui_castro@live.com.pt>
2021-10-07 14:10:25 +0000

    Added a Spinner

---
+++ arvore-da-vida/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "./Spinner.module.css";
+
+const Spinner = () => (
+  <div className={styles.Spinner}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Spinner;
